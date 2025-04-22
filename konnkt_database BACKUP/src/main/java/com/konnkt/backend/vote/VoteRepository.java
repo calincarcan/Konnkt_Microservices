81eb4257e66861bf9f3ef2892c48c324a32f9831
Calin Carcan <94101363+calincarcan@users.noreply.github.com>
2025-04-22 18:07:22 +0300

    Delete konnkt_database BACKUP directory

--- konnkt_database BACKUP/src/main/java/com/konnkt/backend/vote/VoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.konnkt.backend.vote;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
