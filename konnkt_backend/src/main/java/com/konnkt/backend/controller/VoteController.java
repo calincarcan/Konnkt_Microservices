0906ff7e1510b5c4fd907f9a3d0f5e357338c354
Pasca23 <dumitrubogdan2304@gmail.com>
2025-04-26 01:06:22 +0300

    FIX

--- konnkt_backend/src/main/java/com/konnkt/backend/controller/VoteController.java
+++ konnkt_backend/src/main/java/com/konnkt/backend/controller/VoteController.java
@@ -38,6 +38,7 @@ public class VoteController {
     public ResponseEntity<VoteDto> createVote(@RequestBody InputVoteDto inputVoteDto) {
         String url = dbUrl + "/createVote";
         String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        System.out.println("Username: " + username);
         VoteDtoDB voteDtoDB = new VoteDtoDB(
                 username,
                 inputVoteDto.parentType(),
